e6b33a4e6609489bc559592718fe80eda5934f85
Richard Lee <rlee@book.com>
2014-04-21 21:37:08 -0700

    initial import, based on sample of wechat.

---
+++ app/src/main/java/com/weimed/hellowechat/Constants.java
@@ -0,0 +1,12 @@
+package com.weimed.hellowechat;
+
+public class Constants {
+	// APP_ID �滻Ϊ���Ӧ�ôӹٷ���վ���뵽�ĺϷ�appId
+    public static final String APP_ID = "wx5159eae735e97515";
+    
+    public static class ShowMsgActivity {
+		public static final String STitle = "showmsg_title";
+		public static final String SMessage = "showmsg_message";
+		public static final String BAThumbData = "showmsg_thumb_data";
+	}
+}
